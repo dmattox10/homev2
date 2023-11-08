c81528e42ebed647ed4f47afca7ade4c3437671d
Daniel Mattox <dmattox10@gmail.com>
2023-11-08 16:08:38 +0000

    mobile looks better, on the right track

---
+++ front-end/src/components/MobileWin.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect, cloneElement, Children } from "react"
+import cuid from 'cuid'
+const MobileWin = (props) => {
+    const { children, title, visibleSection } = props
+    const [active, setActive] = useState(false)
+    useEffect(() => {
+        if (title === visibleSection) {
+            setActive(true)
+        }
+        else {
+            setActive(false)
+        }
+    }, [title, visibleSection])
+    let dots = []
+    for (let i = 0; i < 3; i++) {
+        dots.push(
+        <div key={cuid()} className="navigator">
+        {
+            active ? <span className="icon red"></span> : <span className="icon grey"></span>
+        }
+        </div>)
+    }
+    return (
+        <div className="main-window">
+                <div className="navigation">
+                    { dots }
+                </div>
+            <div className="window-content">
+                {
+                    Children.map(children, (child) => {
+                        return cloneElement(child, {
+                            active
+                        })
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default MobileWin
--- front-end/src/components/SectionWithObserver.jsx
+++ front-end/src/components/SectionWithObserver.jsx
@@ -1,6 +1,7 @@
 import { useInView } from "react-intersection-observer"
 import { useScreenWidth } from "../hooks/useScreenWidth"
 import Window from "./Window"
+import MobileWin from './MobileWin'
 const SectionWithObserver  = (props) => {
   const { children, setVisibleSection, id, navOpen, visibleSection } = props
   const { ref } = useInView({
@@ -19,9 +20,10 @@ const SectionWithObserver  = (props) => {
   return (
     <section id={id} ref={ref}>
       <div className={isMobile ? navOpen ? "section-body hide" : "section-body" : 'section-body'}>
-        {
-          isMobile ? children : <Window title={id} visibleSection={visibleSection}>{children}</Window>
-        }
+        {isMobile ? 
+			<MobileWin title={id} visibleSection={visibleSection}>{children}</MobileWin> 
+			: 
+			<Window title={id} visibleSection={visibleSection}>{children}</Window>}
       </div>
     </section>
   )
