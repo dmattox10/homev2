e4c660b8b7e6fc3bde9c34d8463b9b8422825e87
Daniel Mattox <dmattox10@gmail.com>
2023-11-08 16:15:07 +0000

    Fixed syntax that would have triggered events on load

--- front-end/src/components/sections/Services.jsx
+++ front-end/src/components/sections/Services.jsx
@@ -5,7 +5,10 @@ import Grow from "../Grow"
 const Services = ({ setVisibleSection, headerRef, active }) => {
     const [service, setService] = useState('0')
     const handleMouseEnter = (e) => {
-        console.log(e)
+        setService(e.target.id)
+    }
+
+    const handleClick = (e) => {
         setService(e.target.id)
     }
 
@@ -21,12 +24,12 @@ const Services = ({ setVisibleSection, headerRef, active }) => {
                 <div className="hero">
                     <div className="block-0 ">
                         <div className="bg-warm">
-                            <div onMouseEnter={handleMouseEnter} id='0' className={service == '0' ? 'big screen' : 'big multiply'}>LEARN</div>
+                            <div onMouseEnter={(e) => handleMouseEnter(e)} onClick={(e) => handleClick(e)} id='0' className={service == '0' ? 'big screen' : 'big multiply'}>LEARN</div>
                         </div>
                     </div>
                     <div className="block-1 bl">
                         <div className="bg-cool bl">
-                            <div onMouseEnter={handleMouseEnter} id='1' className={service == '1' ? 'big screen bl' : 'big multiply bl'}>GROW</div>
+                            <div onMouseEnter={(e) => handleMouseEnter(e)} onClick={(e) => handleClick(e)} id='1' className={service == '1' ? 'big screen bl' : 'big multiply bl'}>GROW</div>
                         </div>
                     </div>
                     <div className="block-2 br">{services[service]}</div>
